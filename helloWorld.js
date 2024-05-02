fd287b6eb22e2436dc810c0955d8a4cdc7c76407
jonaholsen14 <jonaho@byui.edu>
2024-05-02 16:02:47 -0600

    created the site using chat-gpt

---
+++ helloWorld.js
@@ -0,0 +1,9 @@
+// Define two numbers
+const num1 = 5;
+const num2 = 10;
+
+// Add the numbers
+const sum = num1 + num2;
+
+// Display the result
+console.log("The sum of", num1, "and", num2, "is:", sum);
